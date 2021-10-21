ca83fff23e21d9e05d5c5daf8d5ef08e88a9277c
Mauricio Vasco <Maurovasco91@gmail.com>
2021-10-21 12:13:52 -0500

    First commit

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { SourceMapGenerator } from '@angular/compiler/src/output/source_map';
+import { Injectable } from '@angular/core';
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private URL = 'https://reqres.in/api'
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get(`${this.URL}/users?page=2?per_page=6`)
+      .pipe(
+        map(resp => (resp as any)['data'])
+      );
+  }
+}
+
